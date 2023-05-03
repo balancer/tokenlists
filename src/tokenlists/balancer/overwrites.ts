c5ed63dfe8f217f26541a7aa40b8d342f083e3c3
Weston Nelson <dogooddowell@protonmail.com>
2023-05-03 20:56:42 +0000

    Add NFTE token

--- src/tokenlists/balancer/overwrites.ts
+++ src/tokenlists/balancer/overwrites.ts
@@ -275,6 +275,12 @@ export const overwrites: OverwritesForList = {
       logoURI:
         'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
     },
+    '0xB261104A83887aE92392Fb5CE5899fCFe5481456': {
+      name: 'NFTEarth',
+      symbol: 'NFTE',
+      logoURI:
+        'https://assets.coingecko.com/coins/images/29116/large/20230223_224134.jpg?1677224110',
+    },
   },
   [Network.Optimism]: {},
   [Network.Gnosis]: {
