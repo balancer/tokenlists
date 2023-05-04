a8287e84c9f451f0562b173e47f0c4be515d1a7d
tomerXarchimedes <98904111+tomerXarchimedes@users.noreply.github.com>
2023-05-04 14:27:52 -0400

    Adding ARCH token
    archimedesfi.com
    Thanks!

--- src/tokenlists/balancer/tokens.ts
+++ src/tokenlists/balancer/tokens.ts
@@ -2,6 +2,7 @@ import { Network, TokensForList } from '../../types'
 
 export const tokens: TokensForList = {
   [Network.Mainnet]: [
+    '0x73C69d24ad28e2d43D03CBf35F79fE26EBDE1011',
     '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
     '0x6B175474E89094C44Da98b954EedeAC495271d0F',
