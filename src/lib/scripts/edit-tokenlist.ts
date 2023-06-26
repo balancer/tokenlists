8111e4d029ab3336d4c2f2c056f601f1def62afc
Tim Robinson <timjrobinson@users.noreply.github.com>
2023-06-26 15:07:15 +1000

    Add Github workflow to allowlist tokens manually or automatically (#239)
    * Add Github Webhooks to allowlist tokens
    
    * Use main branch
    
    * Add cac to dependencies
    
    * Improve names, use TSX
    
    * Remove eslint check
    
    * Use main instead of master in commit
    
    * Remove options echo, fix lint

---
+++ src/lib/scripts/edit-tokenlist.ts
@@ -0,0 +1,34 @@
+import * as fs from 'fs'
+
+export function allowListToken({
+  network,
+  tokenAddress,
+  tokenSymbol = '',
+}: Record<string, string>) {
+  const filePath = `./src/tokenlists/balancer/tokens/${network}.ts`
+
+  const fileContent = fs.readFileSync(filePath, 'utf-8')
+
+  const regex = new RegExp(`\\[([\\s\\S]+?)\\]`)
+
+  const match = fileContent.match(regex)
+
+  if (!match) throw Error('Token List not found')
+
+  const arrayContent = match[1].trim()
+
+  if (arrayContent.includes(tokenAddress)) {
+    throw Error(`${tokenAddress} is already allowlisted`)
+  }
+
+  let updatedArrayContent = `${arrayContent}\n  '${tokenAddress}',`
+
+  if (tokenSymbol) updatedArrayContent += ` // ${tokenSymbol}`
+
+  // Replaces file with updated content:
+  const updatedContent = fileContent.replace(arrayContent, updatedArrayContent)
+
+  fs.writeFileSync(filePath, updatedContent)
+
+  console.log(`💾 ${network}.ts file updated.`)
+}
