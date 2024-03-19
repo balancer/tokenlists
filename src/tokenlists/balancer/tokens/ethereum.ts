b7dddd7aa1c0b4c3d6332db1cc87def8c32a8c91
balopco <balopco@users.noreply.github.com>
2024-03-19 17:17:53 +0000

    chore: Add rsETH / WETH to ethereum allowlist

--- src/tokenlists/balancer/tokens/ethereum.ts
+++ src/tokenlists/balancer/tokens/ethereum.ts
@@ -894,4 +894,5 @@ export default [
   '0x862c57d48becb45583aeba3f489696d22466ca1b', // stataEthUSDT
   '0x02c2d189b45ce213a40097b62d311cf0dd16ec92', // stataEthUSDC
   '0xfe0c30065b384f05761f15d0cc899d4f9f9cc0eb', // ethfi
+  '0x58AAdFB1Afac0ad7fca1148f3cdE6aEDF5236B6D', // rsETH / WETH
 ]
