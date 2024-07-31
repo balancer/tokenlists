befe070e0519299ee6125dca22cdba879dc1ee6d
franz <franzns11@gmail.com>
2024-07-31 10:52:33 +0200

    zkevm one token a time

--- src/lib/fetchers/onchain.ts
+++ src/lib/fetchers/onchain.ts
@@ -25,7 +25,7 @@ export async function fetchOnchainMetadata(
 
   if (network === Network.Zkevm) {
     // zkevm can only do 195 calls at a time
-    chunks = _.chunk(tokenAddresses, 10)
+    chunks = _.chunk(tokenAddresses, 1)
   } else {
     chunks = _.chunk(tokenAddresses, 200)
   }
