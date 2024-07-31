354e26a3ae65be78834fd02759dc25817b36a736
franz <franzns11@gmail.com>
2024-07-31 10:58:17 +0200

    trust wallet comments

--- src/config/fraxtal.ts
+++ src/config/fraxtal.ts
@@ -4,7 +4,7 @@ export default {
   coingecko: {
     platformId: 'frax',
   },
-  trustWalletNetwork: 'frax',
+  trustWalletNetwork: 'frax', // not a supported chain https://github.com/trustwallet/assets/tree/master/blockchains
   addresses: {
     multicaller: '0xca11bde05977b3631167028862be2a173976ca11',
   },
--- src/config/zkevm.ts
+++ src/config/zkevm.ts
@@ -5,7 +5,7 @@ export default {
   coingecko: {
     platformId: 'polygon-zkevm',
   },
-  trustWalletNetwork: 'polygonzkevm',
+  trustWalletNetwork: 'polygonzkevm', // doesnt have a tokenlist on https://github.com/trustwallet/assets/tree/master/blockchains/polygonzkevm
   addresses: {
     multicaller: '0xca11bde05977b3631167028862be2a173976ca11',
   },
