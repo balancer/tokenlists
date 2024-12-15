ac2dfcce4dec62121a4749ab5ee00a9da89eb680
Ardordocc <122980611+Ardordocc@users.noreply.github.com>
2024-12-15 18:11:20 +0200

    Create sonic.ts
    Added sonic, addresses TBD

---
+++ src/config/sonic.ts
@@ -0,0 +1,11 @@
+export default {
+  name: 'sonic',
+  rpc: `https://lb.drpc.org/ogrpc?network=sonic&dkey=${process.env.DRPC_KEY}`,
+  coingecko: {
+    platformId: 'TBD',
+  },
+  trustWalletNetwork: 'TBD',
+  addresses: {
+    multicaller: 'TBD',
+  },
+}
