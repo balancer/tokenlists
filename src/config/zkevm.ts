4405603f35e09615dcc468587a1bcbb9d3eb1eaa
Vini murafa <06.poems_races@icloud.com>
2025-01-01 17:09:48 +0100

    typo fix Update zkevm.ts

--- src/config/zkevm.ts
+++ src/config/zkevm.ts
@@ -4,7 +4,7 @@ export default {
   coingecko: {
     platformId: 'polygon-zkevm',
   },
-  trustWalletNetwork: 'polygonzkevm', // doesnt have a tokenlist on https://github.com/trustwallet/assets/tree/master/blockchains/polygonzkevm
+  trustWalletNetwork: 'polygonzkevm', // doesn't have a tokenlist on https://github.com/trustwallet/assets/tree/master/blockchains/polygonzkevm
   addresses: {
     multicaller: '0xca11bde05977b3631167028862be2a173976ca11',
   },
