e867d9ee5001a085bd457b93b553de35fe417b1a
Krzysztof Stelmach <kcezarystelmach@gmail.com>
2025-04-11 20:16:19 +0200

    Update tokenlist: DeFi Choice VOTE token

---
+++ src/tokenlists/balancer-tokenlist/metadata.ts
@@ -0,0 +1,10 @@
+export const metadata = {
+  name: 'VOTE',
+  version: {
+    major: 1,
+    minor: 0,
+    patch: 0,
+  },
+  keywords: ['DeFi', 'Governance', 'Voting', 'DeFi Choice', 'Social'],
+  logoURI: 'https://app.defichoice.org/images/token/vote.png',
+};
