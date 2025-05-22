1a3e794e2e01b726d620ca5b9b6bc2bc2e0cbd06
lieft <50590686+lieft@users.noreply.github.com>
2025-05-22 12:34:23 +0800

    Update overwrites.ts

--- src/tokenlists/balancer/overwrites.ts
+++ src/tokenlists/balancer/overwrites.ts
@@ -1753,5 +1753,12 @@ export const overwrites: OverwritesForList = {
       logoURI:
         'https://raw.githubusercontent.com/RealWagmi/tokenlists/main/logos/146/0x0e0Ce4D450c705F8a0B6Dd9d5123e3df2787D16B/logo.png',
     },
+    '0xBB821aca41ac46198931E8fe8468201124fb0ecF': {
+      decimals: 18,
+      symbol: 'BOL',
+      name: 'bollcoin',
+      logoURI:
+        'http://www.bollcoin.com/logo.png',
+    },
   },
 }
