3e2c57d6c1999231cc8d3f504b8f52ee6592fad3
Simão Pinto <87367287+Sk1mer12@users.noreply.github.com>
2025-10-17 14:32:34 +0200

    Whitelist and add hwave token logo

--- src/tokenlists/balancer/tokens/hyperevm.ts
+++ src/tokenlists/balancer/tokens/hyperevm.ts
@@ -51,4 +51,5 @@ export default [
   '0x27ec642013bcb3d80ca3706599d3cda04f6f4452', // upump
   '0x9b498c3c8a0b8cd8ba1d9851d40d186f1872b44e', // purr
   '0x4d0ff6a0dd9f7316b674fb37993a3ce28bea340e', // hyped
+  '0x0E01e3aFd147C7f079eA19d0EcA166Ad3A22e79D', //hwave
 ]
